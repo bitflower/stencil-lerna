9e609c1be2aec76325a4290e0fac2e9598606859
bitflower <matthias.max@bitflower.net>
2019-06-02 23:35:27 +0200

    consume component from design-system

--- packages/app/src/components/app-home/app-home.tsx
+++ packages/app/src/components/app-home/app-home.tsx
@@ -6,21 +6,19 @@ import { Component, h } from '@stencil/core';
   shadow: true
 })
 export class AppHome {
-
   render() {
     return (
-      <div class='app-home'>
+      <div class="app-home">
         <p>
-          Welcome to the Stencil App Starter.
-          You can use this starter to build entire apps all with
-          web components using Stencil!
-          Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
+          Welcome to the Stencil App Starter. You can use this starter to build
+          entire apps all with web components using Stencil! Check out our docs
+          on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
         </p>
 
-        <stencil-route-link url='/profile/stencil'>
-          <button>
-            Profile page
-          </button>
+        <my-component first="Matthias" last="Max" />
+
+        <stencil-route-link url="/profile/stencil">
+          <button>Profile page</button>
         </stencil-route-link>
       </div>
     );
